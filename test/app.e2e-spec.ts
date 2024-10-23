f6d72063580c1541c3e3eeef31e580a28c47a02e
Nikita <zhiendich@gmail.com>
2024-10-23 16:43:06 +0300

    tried to resolved error with e2e test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,14 +2,30 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from 'src/app/app.module';
+import { ConfigService } from '@nestjs/config';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
+  const mockConfigService = {
+    get: jest.fn((key: string) => {
+      if (key === 'JWT_ACCESS_SECRET') {
+        return 'test_access_jwt_secret';
+      }
+      if (key === 'JWT_REFRESH_SECRET') {
+        return 'test_refresh_jwt_secret';
+      }
+      return null;
+    }),
+  };
+
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
-    }).compile();
+    })
+      .overrideProvider(ConfigService)
+      .useValue(mockConfigService)
+      .compile();
 
     app = moduleFixture.createNestApplication();
     await app.init();
